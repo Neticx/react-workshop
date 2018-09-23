21304e6a7c43ddea78307d5290e1e3f25170b7a6
M Fahmi Ulul Azmi <neticxploit@gmail.com>
2018-09-23 15:21:42 +0700

    fix bug

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -3,7 +3,7 @@ import React from 'react'
 const SearchBox = (props) => {
     return (
         <div className="col-md-12">
-            <input type="text" onInput={props.onSearch} className="form-control" placeholder="search something here/>
+            <input type="text" onInput={props.onSearch} className="form-control" placeholder="search something here"/>
         </div>
     )
 }
